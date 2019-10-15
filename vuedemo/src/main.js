83aeadf7ab701d93f52872607f27d0326223bd7b
zhaochenliang <843465095@qq.com>
2019-10-15 19:07:55 +0800

    睿乐购前端vue项目

---
+++ vuedemo/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vant from 'vant';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import { Toast } from 'vant';
+import { Dialog } from 'vant';
+import { Lazyload } from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Toast);
+Vue.use(Dialog);
+Vue.use(Lazyload);
+Vue.use(Vant);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
